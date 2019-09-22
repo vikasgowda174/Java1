6aca2bc2cca05b1f221053a454bab4f00010b2d4
niranjanh92 <55374367+niranjanh92@users.noreply.github.com>
2019-09-22 20:11:47 +0530

    Update TimePrinter.java
    from niranjan

--- src/main/java/org/javaparser/examples/chapter1/TimePrinter.java
+++ src/main/java/org/javaparser/examples/chapter1/TimePrinter.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class TimePrinter {
     
-    public static void main(String args[]){
+        public static void main(String args[]){
         System.out.print(LocalDateTime.now());
     }
 
